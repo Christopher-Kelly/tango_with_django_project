885ceffce106206f03788261978eba5cb7e9328c
Christopher Kelly <2313175k@student.gla.ac.uk>
2019-01-19 16:34:00 +0000

    about working

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -21,10 +21,10 @@ from django.conf.urls import include
 from rango import views
 
 urlpatterns = [
-    url(r'$', views.index, name = 'index'),
+    url(r'^$', views.index, name = 'index'),
     url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^about/', views.about, name = 'about'),
+    url(r'^rango/about/', views.about, name = 'about'),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
