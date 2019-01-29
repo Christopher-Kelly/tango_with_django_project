d1d8a887245659c76b3560562b4cd62108522b10
Christopher Kelly <2313175k@student.gla.ac.uk>
2019-01-29 10:15:28 +0000

    chapter 7

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rango.models import Category, Page
+from rango.forms import CategoryForm
 
 # Create your views here.
 from django.http import HttpResponse
@@ -29,3 +30,14 @@ def show_category(request, category_name_slug):
         context_dict['category'] = None
     return render(request, 'rango/category.html', context_dict)
     
+
+def add_category(request):
+    form = CategoryForm()
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html',{'form':form})
