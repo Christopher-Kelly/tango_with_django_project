65957fdecd0da3bbb55439eacfbbfbb3a8bf9cb3
Christopher Kelly <2313175k@student.gla.ac.uk>
2019-01-31 21:34:40 +0000

    need to finish chapter 10 exercises

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm
+from django.contrib.auth import authenticate, login,logout
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse 
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 from django.http import HttpResponse
@@ -97,16 +101,38 @@ def register(request):
 
 
 
+def user_login(request):
+
+    if request.method='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponse('Your rango account is disabled')
+        else:
+            print('invalid login details {0}, {1}'.format(username,password)
+
+    else:
+        return render(request,'rango/login.html',{})
 
 
 
 
 
 
+@login_restricted
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
 
 
-
-
-
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
 
 
