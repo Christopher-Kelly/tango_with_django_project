9bf1158666c6306d2d45761e73e9ca41c4e5200a
Christopher Kelly <2313175k@student.gla.ac.uk>
2019-02-06 14:01:12 +0000

    deleted password hasher

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -95,8 +95,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
-PASSWORD_HASHERS = ( 'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
-                     'django.contrib.auth.hashers.BCryptPasswordHasher',
+PASSWORD_HASHERS = (
                      'django.contrib.auth.hashers.PBKDF2PasswordHasher',
                      'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',)
 
